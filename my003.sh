205b180f7b2aa1027ec28939c7800b315150a77d
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-07-16 18:32:33 -0400

    Forgot %Y
    this was an error to display year

--- my003.sh
+++ my003.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is a basic bash script.
 # Time date format script
-today=$(date +"%d-%m-Y")
+today=$(date +"%d-%m-%Y")
 time=$(date +"%H:%M:%S")
 printf -v d "Current User:\t%s\nDate:\t\t%s @ %s\n" $USER $today $time
 echo "$d"
