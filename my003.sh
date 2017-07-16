06e67483cc189eb98fa78d8b92089e2966b5002a
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-07-16 18:35:01 -0400

    Added notes
    wiki.bash-hackers.org/commands/builtin/printf

--- my003.sh
+++ my003.sh
@@ -5,3 +5,6 @@ today=$(date +"%d-%m-%Y")
 time=$(date +"%H:%M:%S")
 printf -v d "Current User:\t%s\nDate:\t\t%s @ %s\n" $USER $today $time
 echo "$d"
+
+# format specifiers
+# wiki.bash-hackers.org/commands/builtin/printf
