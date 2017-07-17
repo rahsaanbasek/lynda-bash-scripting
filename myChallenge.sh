28b36cedb26399334e3751dbe807b51d4c9eaada
Rahsaan Basek <rahsaanbasek@gmail.com>
2017-07-16 20:34:18 -0400

    myChallenge.sh
    Chapter challenge

---
+++ myChallenge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is a basic bash script.
+# challenge for Ch.2
+#
+# use some variables, some command substitution, and some string formatting
+#+to make a small summary of some system information. Print the result to 
+#+the screen and write some information to a file.
